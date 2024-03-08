f113f58a966999f8d2d12fcea70a858e689c67f7
Elisee kabore <ekabore@wisesystems.com>
2024-03-08 11:23:44 +0000

    Added information in scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sbatch --output=/scratch/$USER/logs/slurm-%j-%x.out --error=/scratch/$USER/logs/slurm-%j-%x.error sbatch/earthquake.sh ~/scratch/code-snapshots/earthquake
+#Add the following parameters to override the default values if needed
+# --job-name=name_of_the_job       
+# --cpus-per-task=1    
+
+# The last 2 arguments are the sbatch script and the path to the code                                                       
+
+sbatch --mem=1Gb --time:00:05:00 --output=/scratch/$USER/logs/slurm-%j-%x.out --error=/scratch/$USER/logs/slurm-%j-%x.error ~/sbatch/earthquake.sh ~/scratch/code-snapshots/earthquake
